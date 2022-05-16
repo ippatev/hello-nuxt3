065a3f25a731de262cb430038db77caa13272276
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2022-05-17 01:10:49 +0900

    update index vue
    Took 21 minutes

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
-    Hi!
+    <v-row justify="center" align="center">
+      <v-list-subheader>Offset Top {{ offsetTop }}</v-list-subheader>
+    </v-row>
+    <v-container id="scroll-target" style="max-height: 400px" class="overflow-y-auto">
+      <v-row
+          v-scroll:#scroll-target="onScroll"
+          align="center"
+          justify="center"
+          style="height: 1000px"
+      >
+      </v-row>
+    </v-container>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+    offsetTop: 0
+  }
+}
+,
+methods: {
+  onScroll(e)
+  {
+    this.offsetTop = e.target.scrollTop
+  }
+}
+}
+</script>
